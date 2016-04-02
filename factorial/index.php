11dd6d315cd524c0db7f043088fbba03b4ed8129
Samuel Hilson <samrhilson@gmail.com>
2016-04-02 10:44:21 -0500

    fix naming

--- factorial/index.php
+++ factorial/index.php
@@ -5,7 +5,7 @@
  * @param  string  $key  cache key
  * @return void
  */
-function fact_cache($fact, $key)
+function factCache($fact, $key)
 {
 
 }
@@ -14,7 +14,7 @@ function fact_cache($fact, $key)
  * @param  string $key cache key
  * @return integer
  */
-function cache_check($key)
+function cacheCheck($key)
 {
 
 }
@@ -23,7 +23,7 @@ function cache_check($key)
  * @param  integer $i starting number for factorial
  * @return string
  */
-function fact_by_rec($i)
+function factByRec($i)
 {
     if ($i < 0) {
         return false;
@@ -31,7 +31,7 @@ function fact_by_rec($i)
     if ($i < 2) {
         return 1;
     }
-    return $i * factorial($i - 1);
+    return $i * factByRec($i - 1);
 }
 
 /**
@@ -39,7 +39,7 @@ function fact_by_rec($i)
  * @param  integer $n starting number for factorial
  * @return string
  */
-function fact_by_loop($n)
+function factByLoop($n)
 {
     if ($n < 0) {
         return false;
@@ -51,16 +51,16 @@ function fact_by_loop($n)
     return $x;
 }
 
-var_dump(fact_by_rec(4));
-var_dump(fact_by_rec(3));
-var_dump(fact_by_rec(2));
-var_dump(fact_by_rec(0));
-var_dump(fact_by_rec(-1));
-var_dump(fact_by_rec(98));
+var_dump(factByRec(4));
+var_dump(factByRec(3));
+var_dump(factByRec(2));
+var_dump(factByRec(0));
+var_dump(factByRec(-1));
+var_dump(factByRec(98));
 
-var_dump(fact_by_loop(4));
-var_dump(fact_by_loop(3));
-var_dump(fact_by_loop(2));
-var_dump(fact_by_loop(0));
-var_dump(fact_by_loop(-1));
-var_dump(fact_by_loop(171));
+var_dump(factByLoop(4));
+var_dump(factByLoop(3));
+var_dump(factByLoop(2));
+var_dump(factByLoop(0));
+var_dump(factByLoop(-1));
+var_dump(factByLoop(171));
